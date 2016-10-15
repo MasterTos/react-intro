6096d655f13db4016e9bf8ed98af737605a8b222
MasterTos <MasterTos@yahoo.com>
2016-10-15 12:04:57 +0700

    modify

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 var config = {
     entry: {
-        hello: path.join(__dirname, 'src', 'app.js'),
+        hello: path.join(__dirname, 'src', 'app.js')
     },
     module: {
         loaders: [{
